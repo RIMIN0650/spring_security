c9210c46e882cf45b73a3de66d7be8842b6c2a7e
kwon_pc <chris0540@naver.com>
2026-04-21 01:13:50 +0900

    [Feat] 권한 별 접근 페이지 테스트

--- account_be/src/main/java/org/example/account/test/TestController.java
+++ account_be/src/main/java/org/example/account/test/TestController.java
@@ -8,7 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("/ex01")
-    public String test() {
+    public String ex01() {
         return "성공";
     }
+
+    @GetMapping("/ex02")
+    public String ex02() {
+        return "성공";
+    }
+
+    @GetMapping("/ex03")
+    public String ex03() {
+        return "성공";
+    }
+
+    @GetMapping("/ex04")
+    public String ex04() {
+        return "성공";
+    }
+
 }
